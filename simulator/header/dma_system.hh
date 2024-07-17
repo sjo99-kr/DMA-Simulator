09faf68e97cbdc96af01773379bfe41aebef8894
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-17 16:28:20 +0900

    Update dma_system.hh

--- simulator/header/dma_system.hh
+++ simulator/header/dma_system.hh
@@ -1,10 +1,10 @@
 #include "dma_v1.cc"
 
-
-
 class DMA_SYSTEM : public Module {
 	private:
 		DMA_v1* dma;
 	public:
-		DMA_SYSTEM(const std::string& dma_script, const std::string& descriptors, uint8_t* src_addr, uint8_t* dst_addr);
+		DMA_SYSTEM(const std::string& dma_script, const std::string& descriptors,
+		const std::string& mem0_trace, const std::string& mem1_trace, const std::string& mem2_trace, const std::string& mem3_trace );
+
 };
