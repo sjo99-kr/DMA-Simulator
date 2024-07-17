829465eea85cc7f15fc5edc77fb815aec054894b
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-17 16:11:06 +0900

    Update dma_v1.cc

--- simulator/dma_v1.cc
+++ simulator/dma_v1.cc
@@ -1,7 +1,10 @@
 #include "dma_v1.hh"
 #include <cstdint>
+#include <fstream>
 #include <sstream>
-
+#include <vector>
+#include <string>
+#include <iostream>
 
 
 bool DMA_v1::is_busy(){
@@ -20,7 +23,54 @@ void DMA_v1::reset(){
 	clock = 0;
 };
 
-DMA_v1::DMA_v1(const std::string& dma_script){
+uint8_t* DMA_v1::set_memory(const std::string& mem_trace){
+	std::ifstream file(mem_trace);
+	if(!file.is_open()){
+		std::cerr << "Error occurred from mem_trace" << std::endl;
+	}
+	std::string line;
+	std::vector<uint8_t> data;
+	while(std::getline(file, line)){
+		std::istringstream iss(line);
+		std::string value;
+		while(iss>>value){
+			try{
+				uint8_t byte = static_cast<uint8_t>(std::stoul(value, nullptr, 16));
+				data.push_back(byte);
+			
+			} catch(const std::invalid_argument& e){
+				std::cerr<<"invalid value " << value << std::endl;
+			}
+		}
+	}
+	file.close();
+	size_t dataSize = data.size();
+	uint8_t* b = new uint8_t[dataSize];
+	std::copy(data.begin(), data.end(), b);
+	
+	for (size_t i = 0; i < dataSize; ++i) {
+        std::cout << std::dec << static_cast<int>(b[i]) << " ";
+        	if((i!= 0) && (i % 5 == 0)){
+        		std::cout<<std::endl;
+        	}
+    	}
+    	std::cout << std::endl;
+	return b;
+}
+
+
+DMA_v1::DMA_v1(const std::string& dma_script, const std::string& mem0_trace, const std::string& mem1_trace, 
+const std::string& mem2_trace, const std::string& mem3_trace){
+	
+	std::cout<< "memory0 data " << std::endl;
+	mem0 = set_memory(mem0_trace);
+	std::cout<< "memory1 data " << std::endl;	
+	mem1 = set_memory(mem1_trace);
+	std::cout<< "memory2 data " << std::endl;
+	mem2 = set_memory(mem2_trace);
+	std::cout<< "memory3 data " << std::endl;
+	mem3 = set_memory(mem3_trace);
+	
 	std::ifstream inFile(dma_script);
 	if(!inFile) {
 		std::cout << "DMA SCRIPT IS NOT VALID" << std::endl;
@@ -86,7 +136,7 @@ uint32_t DMA_v1::select_channel(){
 	}
 };
 
-bool DMA_v1::solveDescriptors(const std::string& filename, uint8_t* src_addr, uint8_t* dst_addr){
+bool DMA_v1::solveDescriptors(const std::string& filename){
 	bool status;
 	if(is_busy()) return false;
 	
@@ -103,9 +153,8 @@ bool DMA_v1::solveDescriptors(const std::string& filename, uint8_t* src_addr, ui
 	uint32_t burst_size;
 	uint32_t count = 0;
 	uint32_t value;
-	channels[ch_idx].src_addr = src_addr;
-	channels[ch_idx].dst_addr = dst_addr;
-	
+	uint8_t* src_addr;
+	uint8_t* dst_addr;
 	std::string line;
 
 	while(std::getline(infile,line)) {
@@ -123,6 +172,39 @@ bool DMA_v1::solveDescriptors(const std::string& filename, uint8_t* src_addr, ui
 			else if (variableName == "burst_size") {
 				iss >> burst_size;
 			}
+			else if(variableName == "src_device") {
+				std::string value;
+				iss >> value;
+				if(value == "0"){
+					src_addr =  mem0;
+				}
+				else if(value == "1"){
+					src_addr = mem1;
+				}
+				else if(value == "2"){
+					src_addr = mem2;
+				}
+				else if(value == "3"){
+					src_addr = mem3;
+				}
+			}
+			else if(variableName == "dst_device"){
+				std::string value;
+				iss >> value;
+				if(value == "0"){
+					dst_addr =  mem0;
+				}
+				else if(value == "1"){
+					dst_addr = mem1;
+				}
+				else if(value == "2"){
+					dst_addr = mem2;
+				}
+				else if(value == "3"){
+					dst_addr = mem3;
+				}
+			}
+
 		}
 	}
 
