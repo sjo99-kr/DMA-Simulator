69175b35b9227b040c5a33537db2f35c92ba9c9a
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-17 16:27:19 +0900

    Update main.cc

--- simulator/main.cc
+++ simulator/main.cc
@@ -11,20 +11,16 @@
 
 int main(int argc, char* argv[]){
 	std::cout<< "start " << std::endl;
-	if(argc < 3){
-		std::cerr << "Usage : " << argv[0] << " <dma_descript> <descriptors>" << std::endl;
+	if(argc < 7){
+		std::cerr << "Usage : " << argv[0] << " <dma_descript> <descriptors> <mem0_trace> <mem1_trace> <meme2_trace> <mem3_trace>" << std::endl;
 		return 1;
 	}
 	
 	///////////////////////// Memory setting ////////////////////////////////
-	uint8_t RAM[1000];
-	uint8_t peripheral[200];
-	for(uint32_t i =0; i<1000; i++){
-		RAM[i] = i /4;
-	}
-	for(uint32_t i = 0; i<200; i++){
-		peripheral[i] = i;	
-	}
+	std::string mem0_trace = argv[3];
+	std::string mem1_trace = argv[4];
+	std::string mem2_trace = argv[5];
+	std::string mem3_trace = argv[6];
 	/////////////////////////////////////////////////////////////////////////
 	
 	std::string dma_descript = argv[1];
@@ -33,7 +29,7 @@ int main(int argc, char* argv[]){
 	std::cout << "FILE SETTING ON" <<std::endl;
 	
 	// DMA System setting
-	DMA_SYSTEM dma_system(dma_descript, descriptors, RAM, peripheral);
+	DMA_SYSTEM dma_system(dma_descript, descriptors, mem0_trace, mem1_trace, mem2_trace, mem3_trace );
 	
 	return 0;
 
