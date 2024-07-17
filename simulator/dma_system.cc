88f44c7076e823bc3d586a26f16b71256e505e77
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-17 15:16:53 +0900

    Update dma_system.cc

--- simulator/dma_system.cc
+++ simulator/dma_system.cc
@@ -6,6 +6,8 @@
 DMA_SYSTEM::DMA_SYSTEM(const std::string& dma_script , const std::string& descriptors, uint8_t* src_addr, uint8_t* dst_addr){
 
 	dma = new DMA_v1(dma_script);
+
+	
 	dma->solveDescriptors(descriptors, src_addr, dst_addr);
 	
 	std::cout<<"Total Cycles :"  << dma->getClock() << std::endl;
