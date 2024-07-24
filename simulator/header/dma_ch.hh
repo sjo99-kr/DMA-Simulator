b347a5022415b7663689fd41677c33d0aaaadff3
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-24 11:36:36 +0900

    Update dma_ch.hh

--- simulator/header/dma_ch.hh
+++ simulator/header/dma_ch.hh
@@ -10,8 +10,7 @@ class DMA_CHANNEL : public Module {
 		 
 		// Public params 
 		uint32_t transfer_count;  // transfer_count = total transfer size , e.g. 64Bytes , 128Bytes.. 1024Bytes.. 
-		uint8_t transaction_size; // data_width based on bus data-width
-
+		uint32_t transaction_size; // data_width based on bus data-width
 		// Set Channal for transaction (Initailization)
 		void set_channels(uint8_t* src_addr, uint8_t* dst_addr,uint32_t transaction_size, uint32_t transfer_count,
 			bool burst, uint32_t burst_size, uint32_t buf_size);
@@ -20,7 +19,6 @@ class DMA_CHANNEL : public Module {
 		// burst : burst mode or simple mode
 		// burst_size : burst size for burst mode, Note: burst_size < buf_size	
 			
-		
 		void reset();  // Reset DMA Channel parameters  
 		bool is_busy(); // check DMA CHANNEL is busy or not
 		void get_buffer(uint32_t size); // set buffer size based on buf_size parameters.
@@ -36,20 +34,20 @@ class DMA_CHANNEL : public Module {
 		bool dataTransferWrite();
 		
 		// Simple Transfer for Cycle-Stealing Mode (Non-burst Mode)
-		uint32_t SimpleTransfer();
-		uint32_t BurstTransfer();
+		void SimpleTransfer();
+		void BurstTransfer();
 		
 		
 		uint8_t* src_addr; // Source base addr
 		uint8_t* dst_addr; // Destination base addr
 
 		bool burst_or_not; // Burst mode or Cycle-Stealing Mode  (true / false)
-
+		uint32_t channel_id; // set channel ID
 
 	private:
 
 		// private params
-		uint32_t channel_id; // set channel ID
+
 		bool state; // Channel State (IDLE :0  BUSY ; 1)
 		uint32_t buf_reg; // pointer for channel_buffer
 		uint32_t burst_size; // burst_size , num of word for each transfer
