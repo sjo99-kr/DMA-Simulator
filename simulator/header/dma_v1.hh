2ade57231a67af1d0d35a56c6440c5dd92a1f52c
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-30 09:47:05 +0900

    Update dma_v1.hh

--- simulator/header/dma_v1.hh
+++ simulator/header/dma_v1.hh
@@ -1,5 +1,5 @@
 #include <stdint.h>
-#include "dma_ch.cc"
+#include "dma_ch.hh"
 #include <cmath>
 #include <iostream>
 #include <vector>
@@ -7,10 +7,10 @@
 #include <string>
 
 
-class DMA_v1 : public Module // state , mode , transaction_size
+class DMA_v1 // state , mode , transaction_size
 {
 	private:
-		uint8_t state;
+
 		uint32_t num_of_ch;
 		uint32_t addr_width;
 		bool sg_or_not;
@@ -26,18 +26,23 @@ class DMA_v1 : public Module // state , mode , transaction_size
 		
 		
 	public:
-		DMA_v1(const std::string& dma_script, const std::string& mem0_trace, const std::string& mem1_trace, 
-		const std::string& mem2_trace, const std::string& mem3_trace);	
+		DMA_v1(const std::string& dma_script, 
+			   const std::string& descriptors, 
+			   const std::string& mem0_trace, 
+			   const std::string& mem1_trace, 
+			   const std::string& mem2_trace, 
+			   const std::string& mem3_trace);
 
 		// function setting
-		void tick();
 		void reset();
 		void initChannels();
+		bool state;
+				
+		uint32_t reg_channelId; // register for channel_id which is used.
 		
 		uint8_t* set_memory(const std::string& mem_trace);
 		uint32_t getClock(uint32_t channel_id);
 
-		void check(bool status);
 		//
 		bool check_btt(uint32_t transfer_count);
 		uint32_t select_channel();
@@ -45,12 +50,9 @@ class DMA_v1 : public Module // state , mode , transaction_size
 		// Read Descripotr informations and allocate channels.
 		bool solveDescriptors(const std::string& filename);
 			// channel addr -> device id setting
-		
-		// channel active for Read
-		void simpleMode(uint32_t channel_id);
-
-		// channel active for Write
-		void burstMode(uint32_t channel_id);
+			
+		// ACtion processs
+		bool Action(uint32_t clk, bool dst_ready, bool src_ready, bool bus_access);
 		
 		//  DMA Enable or Disable
 		bool is_busy();
