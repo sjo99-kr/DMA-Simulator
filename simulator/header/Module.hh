62fc054e3bf066ac312a57cf9905c45ef574f4c9
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-30 09:46:35 +0900

    Update Module.hh

--- simulator/header/Module.hh
+++ simulator/header/Module.hh
@@ -1,15 +1,22 @@
 #include <stdint.h>
 #include <assert.h>
 #include <iostream>
+#include "dma_v1.hh"
 
-class Module {
+class System_Module {
 
 	protected:
 	public: 
-
-		Module() {}
-		virtual ~ Module() {}
-		uint32_t clock;
+		System_Module(const std::string& dma_descript, const std::string& descriptors,
+		const std::string& mem0_trace, const std::string& mem1_trace, const std::string& mem2_trace, 
+		const std::string& mem3_trace);
+		
+		DMA_v1* DMA_Module;
+		uint32_t clk;
+		bool state;
+		void tick();
+		void clock();
+		bool state_check(bool DMA_State);
 
 };
 
