5ce76e9f9edf47442116faeec5375d7d7a8a8fb2
sjo99 <154411859+sjo99-kr@users.noreply.github.com>
2024-07-30 09:48:28 +0900

    Update main.cc

--- simulator/main.cc
+++ simulator/main.cc
@@ -1,36 +1,37 @@
-#include "dma_system.cc"
-#include <stdlib.h>
-#include <memory>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <cstdint>
+#include "Module.hh"
 
+void System(const std::string& dma_descript, const std::string& descripts, const std::string& mem0, const std::string& mem1, const  std::string&  mem2, const std::string& mem3){
+	// DMA System setting
+	System_Module System_Module(dma_descript, descripts, mem0, mem1, mem2, mem3 );
+	System_Module.clock();
+	
+	std::cout << "************************************************" << std::endl;
+	std::cout << "Total Simulation Time = " << System_Module.clk << " cycle(s)" << std::endl;
+	std::cout << "************************************************" << std::endl;
+
+}
 
 
 
 int main(int argc, char* argv[]){
-	std::cout<< "start " << std::endl;
 	if(argc < 7){
-		std::cerr << "Usage : " << argv[0] << " <dma_descript> <descriptors> <mem0_trace> <mem1_trace> <meme2_trace> <mem3_trace>" << std::endl;
+		std::cerr << "Usage : " << argv[0] << " <dma_descript> <descriptors> <mem0_trace> <mem1_trace> <mem2_trace> <mem3_trace>" << std::endl;
 		return 1;
 	}
 	
 	///////////////////////// Memory setting ////////////////////////////////
-	std::string mem0_trace = argv[3];
-	std::string mem1_trace = argv[4];
-	std::string mem2_trace = argv[5];
-	std::string mem3_trace = argv[6];
+	std::string dma_descript = argv[1]; // DMA Module descriptors
+	std::string descripts = argv[2]; // DMA Channel descriptors
+	std::string mem0 = argv[3];
+	std::string mem1 = argv[4];
+	std::string mem2 = argv[5];
+	std::string mem3 = argv[6];
 	/////////////////////////////////////////////////////////////////////////
 	
-	std::string dma_descript = argv[1];
-	std::string descriptors = argv[2];
-	
-	std::cout << "FILE SETTING ON" <<std::endl;
 	
 	// DMA System setting
-	DMA_SYSTEM dma_system(dma_descript, descriptors, mem0_trace, mem1_trace, mem2_trace, mem3_trace );
-	
+	System(dma_descript, descripts, mem0, mem1, mem2, mem3);
+
 	return 0;
 
 }
